e8bc9e9170ee91fc9945144dd8218ab31d967b70
ArmenHayrapetyan2005 <armen.h24.y@tumo.org>
2022-03-30 22:43:09 +0400

    Initial commit

---
+++ app/src/main/java/com/example/backgroundprocess/PrmaryFragment.kt
@@ -0,0 +1,45 @@
+package com.example.backgroundprocess
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.navigation.fragment.findNavController
+
+
+class PrmaryFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_prmary, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tv_apple_count = view.findViewById<EditText>(R.id.app_count)
+        val tv_apple_max_count = view.findViewById<EditText>(R.id.max_app_count)
+        val button = view.findViewById<Button>(R.id.send)
+
+
+        button.setOnClickListener{
+            if (tv_apple_count.text.length != 0 && tv_apple_max_count.text.length != 0)
+                findNavController()
+                    .navigate(PrmaryFragmentDirections
+                        .actionPrmaryFragmentToSecondaryFragment(
+                            tv_apple_count.text.toString().toInt(),
+                            tv_apple_max_count.text.toString().toInt(),
+                        )
+                    )
+        }
+
+
+    }
+}
